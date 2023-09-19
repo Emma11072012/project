8679340ce73281b75d1dc2f05a53a1db3e14f5cf
Emma11072012 <144716159+Emma11072012@users.noreply.github.com>
2023-09-18 20:15:12 -0400

    Add files via upload

---
+++ 20230917_Chatbot_homework .py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+sport=input('what is your favorite sport? ')
+
+
+# In[2]:
+
+
+print(sport)
+
+
+# In[3]:
+
+
+print('I love sport ', sport)
+
+
+# In[ ]:
+
+
+
+
